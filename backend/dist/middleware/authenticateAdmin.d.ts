0ba0f213b5ddbd40df10a27e6b101641e1e05cdb
Scooper <chuck41090@mac.com>
2025-11-11 14:43:28 -0500

    Update .gitignore to exclude node_modules, dist, and build artifacts

--- backend/dist/middleware/authenticateAdmin.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { User } from '../services/authService';
-export interface AuthenticatedRequest extends Request {
-    user?: User;
-}
-export declare const authenticateAdmin: (req: AuthenticatedRequest, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const authenticateToken: (req: AuthenticatedRequest, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
-//# sourceMappingURL=authenticateAdmin.d.ts.map
--- backend/dist/types/agencyResponse.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * TypeScript interfaces for Agency Response functionality
- * Part of Phase 1C: Basic API Endpoints implementation
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=agencyResponse.js.map
